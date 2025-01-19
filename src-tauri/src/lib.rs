97522ea0f4e782e49d63195fa4b3e544f645a6c2
hrtk91 <ht91218@gmail.com>
2025-01-19 22:22:04 +0900

    feat: ずんだもんがしゃべるようにした

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,97 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use std::{env, sync::Arc};
+
+use tauri::{async_runtime::Mutex, AppHandle};
+pub struct AppState {
+    client: Arc<reqwest::Client>,
+    chat_completion: voicevox_chat::openai::ChatCompletion,
+    sender: mpsc::Sender<Vec<u8>>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn exit(app: AppHandle) {
+    app.exit(0);
+}
+
+#[tauri::command]
+async fn on_send_message(
+    state: tauri::State<'_, Arc<Mutex<AppState>>>,
+    value: String,
+) -> Result<String, String> {
+    let mut state = state.lock().await;
+
+    state.chat_completion.push_user_message(&value);
+
+    let reply = match state.chat_completion.completion().await {
+        Ok(reply) => reply,
+        Err(e) => {
+            eprintln!("Failed to get completion: {:?}", e);
+            Err("Failed to get completion".to_string())?
+        }
+    };
+
+    let wav = voicevox_chat::audio::generate_wav(
+        state.client.clone(),
+        &reply,
+        voicevox_chat::audio::Speakers::Zundamon,
+    )
+    .await;
+
+    if let Ok(wav) = wav {
+        if let Err(err) = state.sender.send(wav).await {
+            eprintln!("Failed to send wav: {:?}", err);
+        }
+    } else {
+        eprintln!("Failed to generate wav");
+    }
+
+    Ok(reply)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    dotenvy::dotenv().map_err(|err| eprintln!("dotenvy error: {}", err)).ok();
+
+    let (sender, receiver) = mpsc::channel(1);
+    let client = Arc::new(reqwest::Client::new());
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![on_send_message, exit])
+        .manage(Arc::new(Mutex::new(AppState {
+            client: client.clone(),
+            chat_completion: voicevox_chat::openai::ChatCompletion::new(
+                env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY is not set"),
+                client.clone(),
+            ),
+            sender
+        })))
+        .setup(|_| {
+            tauri::async_runtime::spawn(async move {
+                async_process(receiver).await
+            });
+            
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+use tokio::sync::mpsc;
+
+async fn async_process(
+    mut receiver: mpsc::Receiver<Vec<u8>>,
+    // mut receiver: mpsc::Receiver<Vec<u8>>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().expect("Failed to get default output stream");
+    let sink = rodio::Sink::try_new(&stream_handle).expect("Failed to create sink");
+    loop {
+        if let Ok(bytes) = receiver.try_recv() {
+            let cursor = std::io::Cursor::new(bytes);
+            let source = rodio::Decoder::new(cursor).expect("Failed to create decoder");
+        
+            sink.append(source);
+        };
+    }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,77 @@
+import { useState } from "react";
 import "./tailwind.css";
+import { invoke } from "@tauri-apps/api/core";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [reply, setReply] = useState("");
+
+  const handleSendMessage = async () => {
+    if (!value) return;
+    setValue("");
+    const reply = await invoke<string>("on_send_message", {
+      value
+    });
+    setReply(reply);
+  };
   return (
-    <main className="h-dvh w-dvw backdrop-blur" data-tauri-drag-region>
-      <div className="flex items-center">
-        <img
-          src="/public/zunda_smile_001.png"
-          alt="zunda"
-          className="max-w-24"
-        />
-        <p className="text-3xl text-white">あいうえお</p>
-      </div>
-    </main>
+    <>
+      <main className="h-dvh w-dvw flex flex-col bg-black bg-opacity-50">
+        <div className="relative w-full grow overflow-y-hidden cursor-grab active:cursor-grabbing">
+          <div className="flex items-center gap-2 select-none h-full w-full p-2">
+            <img
+              src="/public/zunda_smile_001.png"
+              alt="zunda"
+              className="max-w-24"
+            />
+            <div className="overflow-y-auto h-full grow z-20 cursor-auto">
+              <p className="text-base text-white">{reply}</p>
+            </div>
+            <button
+              className="text-white px-2 self-start z-20"
+              type="button"
+              onClick={() => {
+                invoke("exit");
+              }}
+            >
+              X
+            </button>
+            <div
+              className="absolute left-0 right-0 h-full w-full z-10"
+              data-tauri-drag-region
+            />
+          </div>
+        </div>
+        <form
+          className="flex gap-2 px-4 py-2 shrink-0"
+          onSubmit={(ev) => ev.preventDefault()}
+        >
+          <input
+            type="text"
+            className="grow p-2 outline-none"
+            onChange={(ev) => {
+              setValue(ev.target.value);
+            }}
+            value={value}
+            onKeyUp={async (ev) => {
+              // Ctrl + Enterで送信
+              if (ev.ctrlKey && ev.key === "Enter") {
+                await handleSendMessage();
+              }
+            }}
+          />
+          <button
+            type="button"
+            className="text-white px-2 border rounded"
+            onClick={async () => {
+              await handleSendMessage();
+            }}
+          >
+            送信
+          </button>
+        </form>
+      </main>
+    </>
   );
 }
 
